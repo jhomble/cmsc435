b5f318cf14d9ac49239acf1ba20188b4fc47c559
jhomble <jhomble7@gmail.com>
2017-02-19 01:52:52 -0500

    added show and hide

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -11,15 +11,29 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .controller('View2Ctrl', ['$scope', function($scope) {
 
+    // Here are a list of variables to show buttons or change text
+    // My convention right now is e1_1 being elevator 1 on floor 1 
+    $scope.e1_1 = true;
+    $scope.e2_1 = true;
+    $scope.e1_2 = false;
+    $scope.e2_2 = false;
+    $scope.e1_3 = false;
+    $scope.e2_3 = false;
+    $scope.e1_4 = false;
+    $scope.e2_4 = false;
+
+    $scope.e1Action = "text to show elevator 1 action"
+    $scope.e2Action = "text to show elevator 2 action" 
+
     // TODO: Make elevators and calls into objects
     var e1 = {
-	'floor': 1,
-	'direction': 1
+      'floor': 1,
+      'direction': 1
     };
 
     var e2 = {
-	'floor': 1,
-	'direction': 1
+      'floor': 1,
+      'direction': 1
     };
 
     // Sets floor of elevator "closest" to call
@@ -27,21 +41,21 @@ angular.module('myApp.view2', ['ngRoute'])
     this.answerCall = function(fl, dir) {
 
 	var getFS = function(f, d, ef, ed) {
-	    var fs = 4 - Math.abs(ef - f);
+    var fs = 4 - Math.abs(ef - f);
 
-	    if ((ed == 1 && (ef - f) < 0) || (ed == 0 && (ef-f) > 0)){
-		fs = 1;
-	    } else {
-		if (ed == d) {
-		    fs += 2;
-		} else {
-		    fs ++;
-		}
-	    }
+    if ((ed == 1 && (ef - f) < 0) || (ed == 0 && (ef-f) > 0)){
+      fs = 1;
+        } else {
+      if (ed == d) {
+          fs += 2;
+      } else {
+          fs ++;
+      }
+        }
 
-	    return fs;
-	};
-	
+        return fs;
+    };
+
 	var fs1 = getFS(fl,dir, e1.floor, e1.direction);
 	var fs2 = getFS(fl,dir, e2.floor, e2.direction);	    
 
