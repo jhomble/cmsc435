f59a68d4e10f59ff06a1b1e5bc19456df86bbfd9
Ben Mariano <benmariano13@gmail.com>
2017-02-19 19:45:43 -0500

    refactored and changed direction algorithm

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -11,6 +11,9 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .controller('View2Ctrl', ['$scope', function($scope) {
 
+    // Set to false if NOT debugging
+    this.debug = true;    
+
     $scope.e1Action = "text to show elevator 1 action"
     $scope.e2Action = "text to show elevator 2 action" 
 
@@ -25,22 +28,25 @@ angular.module('myApp.view2', ['ngRoute'])
       'direction': 1
     };
 
-    this.isE1AtFloor = function(f) {
-	return f == e1.floor;
-    }
-
-    this.isE2AtFloor = function(f) {
-	return f == e2.floor;
-    }
-
     // Sets floor of elevator "closest" to call
     //    - "closest" determined by FS value
     this.answerCall = function(fl, dir) {
 
+	// NOTE: We should find the following values experimentally!
+	
+	// Amount of time spent during a stop including:
+	//     1) time of door opening/closing
+	//     2) time lost due to slow down of elevator
+	this.TIME_STOPPED = 15;
+	
+	// Amount of time to travel from one floor to the next
+	this.TIME_TO_FLOOR = 7;
+
 	var getFS = function(f, d, ef, ed) {
 	    var fs = 4 - Math.abs(ef - f);
 
-	    if ((ed == 1 && (ef - f) < 0) || (ed == 0 && (ef-f) > 0)){
+	    if ((ed == 1 && f < ef) || (ed == 0 && f > ef) || 
+	        (ed != d) && (f == ef)) {
 		fs = 1;
             } else {
 		if (ed == d) {
@@ -56,21 +62,43 @@ angular.module('myApp.view2', ['ngRoute'])
 	var fs1 = getFS(fl,dir, e1.floor, e1.direction);
 	var fs2 = getFS(fl,dir, e2.floor, e2.direction);	    
 
-	if (fs1 >= fs2) {
-	    if (fl > e1.floor) {
-		e1.direction = 1;
+	// For debugging
+	alert(fs1.toString() + " and " + fs2.toString());
+
+	// Randomly choose if neither is "closer"
+	if (fs1 == fs2) {
+	    var rand = Math.random();
+	    if (rand > .5) {
+		fs1++;
 	    } else {
-		e1.direction = 0;
+		fs2++;
 	    }
+	}
+
+	// Choose elevator that is "closer"
+	if (fs1 > fs2) {
+	    e1.direction = dir;
 	    e1.floor = fl;
 	} else {
-	    if (fl > e2.floor) {
-		e2.direction = 1;
-	    } else {
-		e2.direction = 0;
-	    }
+	    e2.direction = dir;
 	    e2.floor = fl;
-	}
+	} 
+    };
+
+    this.isE1AtFloor = function(f) {
+	return f == e1.floor;
+    }
+
+    this.isE2AtFloor = function(f) {
+	return f == e2.floor;
+    }
+
+    this.getE1Direction = function() {
+	return e1.direction;
+    };
+
+    this.getE2Direction = function() {
+	return e2.direction;
     };
 
     this.getE1Floor = function() {
