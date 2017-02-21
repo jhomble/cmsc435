a2af994f8b5b853e777eaba6a55db112d83ed016
Ben Mariano <benmariano13@gmail.com>
2017-02-20 23:49:56 -0500

    added basic timing, refactored code

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,26 +9,23 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', ['$scope', function($scope) {
+.controller('View2Ctrl', ['$scope', '$interval', function($scope, $interval) {
 
     // Set to false if NOT debugging
-    this.debug = true;    
+    $scope.debug = true;    
 
     $scope.e1Action = "text to show elevator 1 action"
     $scope.e2Action = "text to show elevator 2 action" 
 
-    // TODO: Make elevators and calls into objects
-    var e1 = {
-      'floor': 1,
-      'direction': 1,
-	  'inMotion' : false
+    var Elevator = function () {
+	this.floor = 1.0;
+	this.direction = 0;
+	this.queue = [];
+	this.waitTime = 0;
     };
 
-    var e2 = {
-      'floor': 1,
-      'direction': 1, 
-	  'inMotion' : false
-    };
+    var el1 = new Elevator();
+    var el2 = new Elevator();
 
 	$scope.e1b1 = "btn btn-primary"
 	$scope.e1b2 = "btn btn-primary"
@@ -100,85 +97,145 @@ angular.module('myApp.view2', ['ngRoute'])
 
 	}
 
+    // Advances elevator closer to target if not waiting or idle
+    var updateElevator = function (elevator) {
+	if (elevator.waitTime > 0) {
+	    elevator.waitTime -= 0.01;
+	} else {
+	    var lastIndex = elevator.queue.length-1;
+	    if (Math.abs(elevator.floor - elevator.queue[lastIndex]) <= 0.0001) {
+		elevator.floor = elevator.queue[lastIndex];
+		elevator.queue.pop();
+		elevator.waitTime = 1;
+	    }
+	    
+	    if (elevator.queue.length == 0) {
+		elevator.direction = 0;
+	    } else if (elevator.waitTime <= 0) {
+		if (elevator.direction == 1) {
+		    elevator.floor += 0.01;
+		} else if (elevator.direction == -1) {
+		    elevator.floor -= 0.01;
+		}
+	    }
+	}
+		
+    };
+
+    $interval(function() {
+	updateElevator(el1);
+    }, 100);
+    
+    $interval(function() {
+	updateElevator(el2);
+    }, 100);
+
     // Sets floor of elevator "closest" to call
     //    - "closest" determined by FS value
     this.answerCall = function(fl, dir) {
 
-		// NOTE: We should find the following values experimentally!
-		
-		// Amount of time spent during a stop including:
-		//     1) time of door opening/closing
-		//     2) time lost due to slow down of elevator
-		this.TIME_STOPPED = 15;
-		
-		// Amount of time to travel from one floor to the next
-		this.TIME_TO_FLOOR = 7;
-
-		var getFS = function(f, d, ef, ed) {
-			var fs = 4 - Math.abs(ef - f);
-
-			if ((ed == 1 && f < ef) || (ed == 0 && f > ef) || 
-				(ed != d) && (f == ef)) {
-			fs = 1;
-				} else {
-			if (ed == d) {
-				fs += 2;
-			} else {
-				fs ++;
-			}
-				}
-
-				return fs;
+	        var getDirection = function(f, ef) {
+		    if (ef > f) {
+			return -1;
+		    } else if (ef < f) {
+			return 1;
+		    } else {
+			return 0;
+		    }
 		};
 
-		var fs1 = getFS(fl,dir, e1.floor, e1.direction);
-		var fs2 = getFS(fl,dir, e2.floor, e2.direction);	    
+		var getFS = function(f, d, e) {
+			var fs = 4 - Math.abs(e.floor - f);
+
+			if ((e.direction == 1 && f < e.floor) || 
+			    (e.direction == 0 && f > e.floor) || 
+			    (e.direction != d) && (f == e.floor)) {
+			    fs = 1;
+			} else {
+			    if (e.direction == d) {
+				fs += 2;
+			    } else {
+				fs ++;
+			    }
+			}
+		    
+		    return fs;
+		};
+
+		var fs1 = getFS(fl, dir, el1);
+		var fs2 = getFS(fl, dir, el2);	    
 
 		// For debugging
-		alert(fs1.toString() + " and " + fs2.toString());
+		//alert(fs1.toString() + " and " + fs2.toString());
 
 		// Randomly choose if neither is "closer"
 		if (fs1 == fs2) {
-			var rand = Math.random();
-			if (rand > .5) {
+		    var rand = Math.random();
+		    if (rand > .5) {
 			fs1++;
-			} else {
+		    } else {
 			fs2++;
-			}
+		    }
 		}
+
+
+	        // Adds call to elevator queue and adjusts direction
+	        var registerElevator = function(elevator) {
+	            var newDir = 0;
+		    if (elevator.queue.length == 0) {
+			newDir = getDirection(fl, elevator.floor);
+		    } else {
+			newDir = elevator.direction;
+		    }
+		    if (elevator.queue.indexOf(fl) == -1 && elevator.floor != fl) {
+			elevator.queue.push(fl);
+			elevator.queue.sort();
+			if (newDir == 1) {
+			    elevator.queue.reverse();
+			}
+		    }
+		    elevator.direction = newDir;
+		    
+		};
 
 		// Choose elevator that is "closer"
 		if (fs1 > fs2) {
-			e1.direction = dir;
-			e1.floor = fl;
+		    registerElevator(el1);
 		} else {
-			e2.direction = dir;
-			e2.floor = fl;
+		    registerElevator(el2);
 		} 
     };
 
     this.isE1AtFloor = function(f) {
-	return f == e1.floor;
+	return f == el1.floor;
     }
 
     this.isE2AtFloor = function(f) {
-	return f == e2.floor;
+	return f == el2.floor;
     }
 
+    this.getE1Queue = function() {
+	return el1.queue;
+    };
+
+    this.getE2Queue = function() {
+	return el2.queue;
+    };
+
     this.getE1Direction = function() {
-	return e1.direction;
+	return el1.direction;
     };
 
     this.getE2Direction = function() {
-	return e2.direction;
+	return el2.direction;
     };
 
     this.getE1Floor = function() {
-	return e1.floor;
+	return el1.floor;
     };
 
     this.getE2Floor = function() {
-	return e2.floor;
+	return el2.floor;
     };
 
 }]);
